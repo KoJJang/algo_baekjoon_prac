db702317024ba1348ceafb44f6aa5ab1a4d83159
장성영 <jangseong-yeong@jangseong-yeong-ui-MacBook-Pro.local>
2019-03-21 20:46:14 +0900

    catalan_dp

---
+++ 4811_catalan_dp.cpp
@@ -0,0 +1,30 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//int N;
+//unsigned long long dp[31];
+//
+//int main(){
+//    cin.tie(0);
+//    ios::sync_with_stdio(0);
+//    
+//    N = 1;
+//    
+//    dp[0] = 1; dp [1] = 1;
+//    for(int i = 2; i < 31; i++){
+//        int t = 0; unsigned long long tmp = 0;
+//        while(t<i){
+//            tmp += dp[i-t-1]*dp[t];
+//            t++;
+//        }
+//        dp[i] = tmp;
+//    }
+//    
+//    while(N != 0){
+//        cin >> N;
+//        if(N==0) return 0;
+//        cout << dp[N] << "\n";
+//    }
+//}
+
